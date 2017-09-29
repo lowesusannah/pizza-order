651772296c36cc2cb6220c8be93afb13ebeca270
Susannah Lowe <lowesusannah@gmail.com>
2017-09-29 13:44:23 -0700

    write Pizza object using  constructor notation and prototype function

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,27 @@
 //business logic
+function Pizza(toppings, size) {
+  this.toppings = toppings,
+  this.size = size
+};
+
+Pizza.prototype.findCost = function(toppings, size) {
+  return this.topings + this.size;
+  console.log(toppings, size, totalCost);
+};
 
 
 //user-interface logic
 $(document).ready(function(){
   $("form#pizza-form").submit(function(event){
     event.preventDefault();
-    var pizzaSizeCost = parseInt($("#pizza-size").val());
-    var pizzaToppingCost = parseInt($(this).val());
-    $("input:checkbox [name=toppings]:checked").each(function(){
+    var toppings = parseInt($("#pizza-size").val());
+    var size = $("input:checkbox [name=toppings]:checked").each(function(){
+      return parseInt($(this).val());
     });
-    var totalCost = pizzaToppingCost + pizzaSizeCost;
-    $("#total-cost").append("$" + totalCost + "<br>");
+    var newPizza = new Pizza(toppings, size);
+    var finalPrice = newPizza.findCost(newPizza);
     $("#total-cost").show();
+    $("#total-cost").append("$" + finalPrice + "<br>");
     $("form#pizza-form").hide();
   });
 });
