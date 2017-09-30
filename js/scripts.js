235a0b033beae3d5543c3fe5d855bd98f53343cb
Susannah Lowe <lowesusannah@gmail.com>
2017-09-29 17:28:18 -0700

    correctly tally the total of pizza size + 1 topping

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,36 @@
 //business logic
-function Pizza(toppings, size) {
-  this.toppings = [],
-  this.size = size
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = [];
 };
 
-Pizza.prototype.findCost = function(toppings, size) {
-  return this.topings + this.size;
-};
+function Toppings(pepperoni, mushrooms, extraCheese) {
+  this.pepperoni = pepperoni;
+  this.mushrooms = mushrooms;
+  this.cheese = extraCheese;
+  };
 
+Pizza.prototype.findCost = function(size, toppings) {
+  var toppingLength = [this.toppings].length;
+  return this.size + toppingLength;
+};
 
 //user-interface logic
 $(document).ready(function(){
   $("form#pizza-form").submit(function(event){
     event.preventDefault();
     var size = parseInt($("#pizza-size").val());
-    var toppingsInput = $("input:checkbox[name=toppings]:checked").val();
-    var toppings = parseInt(toppingsInput);
 
-    //var newPizza = new Pizza(toppingsInput, size);
-    //var finalPrice = newPizza.findCost();
+    var toppings = $("input:checkbox[name=toppings]:checked").each(function() {
+      var pepperoni = $(this).find("input#pepperoni").val();
+      var mushrooms = $(this).find("input#mushrooms").val();
+      var extraCheese = $(this).find("input#extra-cheese").val();
+      var toppings = parseInt(pepperoni + mushrooms + extraCheese);
+      });
+    var newPizza = new Pizza(size, toppings);
+    var finalPrice = newPizza.findCost(size, toppings);
     $("#total-cost").show();
-    //$("#total-cost").append("$" + finalPrice + "<br>");
+    $("#total-cost").append("$" + finalPrice + "<br>");
     $("form#pizza-form").hide();
   });
 });
